d2e992ab2b33ecf43b43495edde20c1ea44586d2
seif-eddine-mouihbi <mouihbiseif@gmail.com>
2024-11-20 01:05:36 +0100

    add query

--- backend/controllers/hotel.js
+++ backend/controllers/hotel.js
@@ -1,45 +1,16 @@
 import Hotel from '../models/Hotel.js';
 
 export const getHotels = async (req, res, next) => {
+  const { min, max, limit, ...others } = req.query;
+  // console.log('limit:', limit); //Debugging Statment
   try {
-    const findHotels = await Hotel.find();
+    const findHotels = await Hotel.find({
+      ...others,
+      cheapsetPrice: { $gte: min | 1, $lte: max || 9999 },
+    }).limit(limit);
     res.status(200).json(findHotels);
   } catch (err) {
-    next(err);
-  }
-};
-
-export const countByType = async (req, res, next) => {
-  try {
-    const hotelCount = await Hotel.countDocuments({ type: 'hotel' });
-    const apartmentCount = await Hotel.countDocuments({ type: 'apartment' });
-    const resortCount = await Hotel.countDocuments({ type: 'resort' });
-    const villaCount = await Hotel.countDocuments({ type: 'villa' });
-    const cabinCount = await Hotel.countDocuments({ type: 'cabin' });
-    res.status(200).json([
-      { type: 'hotels', count: hotelCount },
-      { type: 'apartments', count: apartmentCount },
-      { type: 'resorts', count: resortCount },
-      { type: 'villas', count: villaCount },
-      { type: 'cabins', count: cabinCount },
-    ]);
-  } catch (err) {
-    next(err);
-  }
-};
-
-// we gonna search of hotels by cities and counted , next we add the count number in the client side
-export const countByCity = async (req, res, next) => {
-  const cities = req.query.cities.split(',');
-
-  try {
-    const list = await Promise.all(
-      cities.map((city) => {
-        return Hotel.countDocuments({ city: city });
-      })
-    );
-    res.status(200).json(list);
-  } catch (err) {
+    console.error(err);
     next(err);
   }
 };
@@ -80,6 +51,42 @@ export const deleteHotel = async (req, res, next) => {
   try {
     await Hotel.findByIdAndDelete(req.params.id);
     res.status(200).json('Hotel has been deleted.');
+  } catch (err) {
+    next(err);
+  }
+};
+
+// we gonna filter hotels by type and counted them, next we add the count number & type in the client side
+export const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: 'hotel' });
+    const apartmentCount = await Hotel.countDocuments({ type: 'apartment' });
+    const resortCount = await Hotel.countDocuments({ type: 'resort' });
+    const villaCount = await Hotel.countDocuments({ type: 'villa' });
+    const cabinCount = await Hotel.countDocuments({ type: 'cabin' });
+    res.status(200).json([
+      { type: 'hotels', count: hotelCount },
+      { type: 'apartments', count: apartmentCount },
+      { type: 'resorts', count: resortCount },
+      { type: 'villas', count: villaCount },
+      { type: 'cabins', count: cabinCount },
+    ]);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// we gonna search of hotels by cities and counted , next we add the count number in the client side
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(',');
+
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
   } catch (err) {
     next(err);
   }
